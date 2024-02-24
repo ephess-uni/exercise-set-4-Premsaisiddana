3d01da970828644dfad39a34e82fb1649cd3725b
Premsaisiddana <psiddana@cbu.edu>
2024-02-23 23:02:48 -0600

    Update ex_4_2.py
    Completed

--- src/ex_4_2.py
+++ src/ex_4_2.py
@@ -2,7 +2,6 @@ a""" ex_4_2.py """
 from datetime import datetime
 
 def logstamp_to_datetime(datestr):
-
     return datetime.strptime(datestr, '%Y-%m-%dT%H:%M:%S')
 if __name__ == "__main__":
     test_date = '2022-12-01T01:02:03'
