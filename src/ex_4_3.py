8f47a07362046a1197cfa6f35f3ca12df6fa86a2
Premsaisiddana <psiddana@cbu.edu>
2024-02-23 23:09:00 -0600

    Update ex_4_3.py
    Completed

--- src/ex_4_3.py
+++ src/ex_4_3.py
@@ -10,18 +10,22 @@ except ImportError:
     from ex_4_2 import logstamp_to_datetime
     from util import get_data_file_path
 
-# Use this FILENAME variable to test your function.
 FILENAME = get_data_file_path("messages.log")
-# >>>> DO NOT MODIFY CODE ABOVE <<<<
 
 
 def time_between_shutdowns(logfile):
-    """
-    Your docstring here.  Replace the pass keyword below with your implementation.
-    """
-    pass
 
+    shutdown_entries = get_shutdown_events(logfile)
+    if not shutdown_entries:
+        return None  # Return None if no shutdown events are found
 
-# >>>> The code below will call your function and print the results
+    first_shutdown = logstamp_to_datetime(shutdown_entries[0].split()[1])
+    last_shutdown = logstamp_to_datetime(shutdown_entries[-1].split()[1])
+
+    time_difference = last_shutdown - first_shutdown
+
+    return time_difference
 if __name__ == "__main__":
     print(f'{time_between_shutdowns(FILENAME)=}')
+
+
