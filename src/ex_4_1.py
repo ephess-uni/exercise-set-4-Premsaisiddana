e433e9a59dc1062206387990360c24cfc243d4dc
Premsaisiddana <psiddana@cbu.edu>
2024-02-23 22:57:32 -0600

    Update ex_4_1.py
    Completed

--- src/ex_4_1.py
+++ src/ex_4_1.py
@@ -8,18 +8,15 @@ except ImportError:
     from ex_4_0 import get_shutdown_events
     from util import get_data_file_path
 
-# Use this FILENAME variable to test your function.
 FILENAME = get_data_file_path('messages.log')
-# >>>> DO NOT MODIFY CODE ABOVE <<<<
-
 
 def num_shutdowns(logfile):
-    """
-    Your docstring here.  Replace the pass keyword below with your implementation.
-    """
-    pass
+    shutdown_lines = get_shutdown_events(logfile)
+    num_shutdowns = 0
+    for line in shutdown_lines:
+        if "Shutdown initiated" in line or "Shutdown complete" in line:
+            num_shutdowns += 1
+    return num_shutdowns
 
-
-# >>>> The code below will call your function and print the results
 if __name__ == "__main__":
     print(f'{num_shutdowns(FILENAME)=}')
